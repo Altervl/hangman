29c1498d7b7d03c004737b9fb9b2e62bb0a35cde
Altervl <62514019+Altervl@users.noreply.github.com>
2024-12-08 18:09:55 +0300

    Implement Game#play method

--- lib/game.rb
+++ lib/game.rb
@@ -2,19 +2,53 @@
 
 require_relative 'computer'
 require_relative 'player'
+require_relative 'display'
 
 # Main class of the project, holds the entire state of the game and results
 class Game
+  attr_reader :player, :computer, :display, :word, :state
+  attr_accessor :attempts, :hidden, :revealed, :message
+
   def initialize
     @player = Player.new
     @computer = Computer.new
-    @word = nil
+    @display = Display.new
     @attempts = 7
     @hidden = @word.split ''
     @revealed = []
+    @message = 'Try a letter!'
+    @state = [attempts, hidden, revealed, message]
   end
 
   def play
-    # to do
+    @word = computer.pick_word
+
+    display.show state
+
+    while attempts.positive?
+      letter = player.guess
+
+      process letter
+
+      if hidden.empty?
+        @message = 'Congratulations, you won!'
+
+        display.show state
+        exit
+      end
+
+      display.show state
+    end
+  end
+
+  def process(guess)
+    if word.include? guess
+      revealed << guess
+      hidden.delete guess
+      @message = 'Correct!'
+    else
+      @attempts -= 1
+      @message = 'Incorrect. Next try!'
+    end
   end
 end
